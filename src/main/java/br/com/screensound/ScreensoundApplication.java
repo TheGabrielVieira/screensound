3016cb5cf52a4706c8e93c0444f64ad169e7c2df
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-03 03:38:54 -0300

    first commit

---
+++ src/main/java/br/com/screensound/ScreensoundApplication.java
@@ -0,0 +1,21 @@
+package br.com.screensound;
+
+import br.com.screensound.main.Main;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreensoundApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreensoundApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Main main = new Main();
+        main.exibeMenu();
+
+    }
+}
