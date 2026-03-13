8f3d17f628e7c04f6cbce7edbfb0d2527f2069d5
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-13 13:08:46 -0300

    feat: adicionando novos metdos na função principal, adicionando a camada de repository para artista e fazendo a busca da musica por artista com jpql e adicionando enum para tipar os artistas

--- src/main/java/br/com/screensound/main/Main.java
+++ src/main/java/br/com/screensound/main/Main.java
@@ -1,10 +1,23 @@
 package br.com.screensound.main;
 
+import br.com.screensound.model.Artista;
+import br.com.screensound.model.Musica;
+import br.com.screensound.model.TipoArtista;
+import br.com.screensound.repository.ArtistaRepository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
 
+    private final ArtistaRepository repositorio;
+
     private Scanner scanner = new Scanner(System.in);
+
+    public Main(ArtistaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void exibeMenu() {
         var opcao = -1;
@@ -52,17 +65,53 @@ public class Main {
     }
 
     private void pesquisarDadosDoArtista() {
+        System.out.println("Pesquisar dados sobre qual artista? ");
+        var nome = scanner.nextLine();
+        var resposta = consultaChatGPT.obterInformacao(nome);
+        System.out.println(resposta.trim());
     }
 
     private void buscarMusicasPorArtista() {
+        System.out.println("Pesquisar musicas de que artista?");
+        var nome = scanner.nextLine();
+        List<Musica> musicas = repositorio.buscaMusicasPorArtista(nome);
+        musicas.forEach(System.out::println);
     }
 
     private void listarMusicas() {
+        List<Artista> artistas = repositorio.findAll();
+        artistas.forEach(a -> a.getMusicas().forEach(System.out::println));
     }
 
     private void cadastrarMusicas() {
+        System.out.println("Cadastrar musicas de que artista?");
+        var nome = scanner.nextLine();
+        Optional<Artista> artista = repositorio.findByNomeContainingIgnoreCase(nome);
+        if (artista.isPresent()) {
+            System.out.println("Informe o titulo da música:");
+            var nomeMusica = scanner.nextLine();
+            Musica musica = new Musica(nomeMusica);
+            musica.setArtista(artista.get());
+            artista.get().getMusicas().add(musica);
+            repositorio.save(artista.get());
+        } else {
+            System.out.println("Artista não encontrado.");
+        }
     }
 
     private void cadastrarArtistas() {
+        var cadastrarNovo = "S";
+
+        while (cadastrarNovo.equalsIgnoreCase("S")) {
+            System.out.println("Digite o nome do artista:");
+            var nome = scanner.nextLine();
+            System.out.println("Informe o tipo de artista (solo, dupla, banda):");
+            var tipo = scanner.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+            Artista artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+            System.out.println("Cadastrar novo artista? (S/N)");
+            cadastrarNovo = scanner.nextLine();
+        }
     }
 }
